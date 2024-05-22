7f545eb946a2767454601f54dc75f69c9c333195
dengerraju <dengerraju80@gmail.com>
2024-05-22 23:04:51 +0530

    Update main.c

--- HelloWorld/main.c
+++ HelloWorld/main.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/*
- * @author jelathro
- * @date 2/18/13
- * 
- * Print "Hello, World!" to the console
- */
-int main(void){
-    printf("Hello, World!\n");
+int main()
+{
+    printf("Hello world!\n");
     return 0;
 }
